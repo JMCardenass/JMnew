5c04a0af706f6a0ebcd1d7fb1b8c6f19a2fa675f
JMCardenass <jmcardenas1807@gmail.com>
2023-04-26 14:03:23 -0500

    feat: Actividades

---
+++ Mayor de edad.py
@@ -0,0 +1,4 @@
+print("Ingrese su edad")
+edad = int(input())
+if edad >= 18:
+    print("Usted es mayor de edad")
---
+++ Paryimpar.py
@@ -0,0 +1,6 @@
+print("Un numero entero")
+num = int(input())
+if num % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
